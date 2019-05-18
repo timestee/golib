cfd2afe93f920f84a1ae5390109738131fa5caaa
timestee <wanghuidev@gmail.com>
2019-05-18 20:39:49 +0800

    add nocopy

---
+++ nocopy/nocopy.go
@@ -0,0 +1,16 @@
+package nocopy
+
+// Dummy should be embedded into a struct which mustn't be copied.
+// This doesn't prevent copying, but will cause `go vet` to
+// show a warning.
+// Usual implementations would be:
+//
+// 	type DoNotCopyThis struct {
+//		noCopy nocopy.Dummy
+// 	}
+type Dummy struct{}
+
+// Lock sets the lock method to something `go vet` will
+// understand. For details, see
+// https://github.com/golang/go/issues/8005#issuecomment-190753527.
+func (*Dummy) Lock() {}
